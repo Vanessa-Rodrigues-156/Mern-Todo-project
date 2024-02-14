a77366ad32983ecd08e49e99bf4444f9233ad3e5
Vanessa <vanessarodrigues010506@gmail.com>
2024-02-14 15:34:33 +0530

    indexhtml location changed

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-//MONGO_URI = "mongodb+srv://ClusterV1:dbv@clusterv1.pzzsidx.mongodb.net/?retryWrites=true&w=majority";
+
 const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
